ec1ccf70ede6b7299f59c212eacb5ee4b45dff08
Sebastian <tassone_af@yahoo.com>
2021-07-14 17:39:22 -0300

    Create: action to get city weather

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { getWather } from './utils';
+
+function App() {
+
+  // getWather('avellaneda').then((data) => console.log(data))
+
+  return (
+    <div className="App">
+      <p>asdasd</p>
+    </div>
+  );
+}
+
+export default App;
