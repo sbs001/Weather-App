f1d0796e78c597414694237efffd09d62728d49f
Sebastian <tassone_af@yahoo.com>
2021-07-15 17:23:09 -0300

    add: weather card

---
+++ client/src/Components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card({city}){
+
+  return (
+    <div className='card'>
+      <div className="head">
+        <img src={`https://openweathermap.org/img/wn/${city.weather.icon}@2x.png`} alt="" />
+      <span>{ Math.round(city.main.temp) - 273 }°</span>
+      </div>
+      <h1>{city.name}</h1>
+
+    </div>
+  )
+}
