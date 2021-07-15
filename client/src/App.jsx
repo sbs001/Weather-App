e7d919b7f2aefa4ea5b61f06ad1d64ddfc690d56
Sebastian <tassone_af@yahoo.com>
2021-07-15 11:19:45 -0300

    edit: reorganizate nav. Quit Searchbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,14 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 import Nav from './Components/Nav/Nav';
-// import { getWather } from './utils';
 
 function App() {
 
-  // getWather('avellaneda').then((data) => console.log(data))
 
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-        {/* <Route path='*' component={Nav} /> */}
         <Route exact path='/' component={Home} />
       </BrowserRouter>
     </div>
--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css'
 import logo from '../../logo.svg'
-import SearchBar from './SearchBar/SearchBar';
 
 export default function Nav() {
 
   return (
-    <div className='navCtn'>
-      <div className='nav'>
-        <div className='navLeft'>
-          <Link to='/'><img src={logo} alt="" style={{ width: 50 }} /></Link>
-          <Link to='/'> Home</Link>
-          <Link to='about'> About Me </Link>
-        </div>
-        <div className="navRight">
-          <SearchBar />
-        </div>
-      </div>
+    <div className='nav'>
+      <Link to='/'><img src={logo} alt="" style={{ width: 50 }} /></Link>
+      <Link to='/'> Home</Link>
+      <Link to='about'> About Me </Link>
     </div>
   )
 }
