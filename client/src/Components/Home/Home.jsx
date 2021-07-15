ab120eae10923ce6ac11d8c6239dcc2728be0a48
Sebastian <tassone_af@yahoo.com>
2021-07-15 15:32:24 -0300

    Add: home styles and app background

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import SearchBar from '../SearchBar/SearchBar';
+import './Home.css'
 
-export default function Home (){
 
-  return(
-    <div>
-      Home
+export default function Home() {
+
+  const cities = useSelector(state => state.cities);
+
+
+  return (
+    <div className="home">
+      <div className="cities">
+        {!cities.length && <h1>Wellcome to WeatherApp</h1>}
+      </div>
+      <div className="searchCtn">
+        <SearchBar />
+      </div>
     </div>
   )
 }
