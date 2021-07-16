e63e0557a82553a00a5b3e01d79543108a0fb882
Sebastian <tassone_af@yahoo.com>
2021-07-16 01:06:12 -0300

    Edit: animations on nav

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css'
-import logo from '../../logo.svg'
+import logo from '../../img/sol.png'
 
 export default function Nav() {
 
   return (
     <div className='navCtn'>
-      <Link to='/'><img src={logo} alt="" style={{ width: 50 }} /></Link>
-      <Link to='/'> Home</Link>
-      <Link to='about'> About Me </Link>
+      <Link className="aAnimated1s " to='/'><img className="fromLeft" src={logo} alt="" style={{ width: 50 }} /></Link>
+      <Link className="aAnimated2s" to='/'> Home</Link>
+      <Link className="aAnimated3s" to='about'> About Me </Link>
     </div>
   )
 }
